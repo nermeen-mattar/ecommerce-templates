21c1a5d22434b8166814487159ccb6c7a22ce2dc
Nermeen Mattar <v-nmattar@expedia.com>
2020-12-19 20:51:54 +0200

    Attaching listeners to add to cart and profile toggle

---
+++ js/wl-custom.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function() {
+    $('#add_to_cart').on('click', function(){
+        // update local storage
+        // update cart
+    });
+
+    $('.profile_toggle').on('click', function(){
+        // dispay signin modal
+    });
+});
